7b331a52b72670d7fb453e3d3b9a8eabf06801b5
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-05-19 12:30:07 +0200

    création de l'interface joueur

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr21/jeuQuizz/services/interfaces/IJoueurServices.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr21/jeuQuizz/services/interfaces/IJoueurServices.java
@@ -1,8 +1,35 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.services.interfaces;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.entities.dto.JoueurDTO;
+import java.util.List;
 
-public class IJoueurServices {
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.utils.exceptions.ajouterJoueurDTOExceptions.*;
+        import org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.utils.exceptions.ajouterJoueurDTOExceptions.pseudoExceptions.*;
+        import org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.utils.exceptions.ajouterJoueurDTOExceptions.anneeNaissanceExceptions.*;
+        import org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.utils.exceptions.ajouterJoueurDTOExceptions.centresInteretsExceptions.*;
 
-    public void ajouterJoueurDTO() throws Exception {}
+public interface IJoueurServices {
 
-    public void afficherJoueurDTO() throws Exception {}
+    JoueurDTO ajouterJoueur(
+            String pseudo,
+            String prenom,
+            int anneeNaissance,
+            List<String> centresInterets,
+            String languePreferee
+    ) throws AjouterJoueurDTOException,
+            PseudoException,
+            PseudoExistantException,
+            PseudoCommencantParUnChiffreException,
+            NomJoueurDTOException,
+            AnneeNaissanceException,
+            AnneeNaissNegativeException,
+            AnneeNaissanceFuturException,
+            CentresInteretsException,
+            CentresIntDoublonsException,
+            LangagePrefereException;
+
+    List<JoueurDTO> listeJoueurs()
+            throws Exception; // Tu peux spécifier ici les vraies exceptions utilisées par listeJoueurs si connues
+
+    void supprimerJoueur(String pseudo)
+            throws Exception; // À remplacer par une exception comme JoueurNonExistantException si elle existe
 }
--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr21/jeuQuizz/utils/exceptions/ajouterJoueurDTOExceptions/AjouterJoueurDTOException.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr21/jeuQuizz/utils/exceptions/ajouterJoueurDTOExceptions/AjouterJoueurDTOException.java
@@ -1,6 +1,6 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr21.jeuQuizz.utils.exceptions.ajouterJoueurDTOExceptions;
 
-public class AjouterJoueurDTOException {
+public class AjouterJoueurDTOException extends Exception {
     public AjouterJoueurDTOException(String message) {
         System.out.println(message);
     }
